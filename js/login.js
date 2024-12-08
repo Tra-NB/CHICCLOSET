e1e1d80b083a4523451b605e0875593aadffdd43
Tra-NB <tran.ngocbao.tran.jb@gmail.com>
2024-12-08 13:48:23 +0700

    Initial commit

---
+++ js/login.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const container = document.getElementById('container');
+    const registerBtn = document.getElementById('register');
+    const loginBtn = document.getElementById('login');
+
+    if (registerBtn && container) {
+        registerBtn.addEventListener('click', () => {
+            container.classList.add("active");
+        });
+    }
+
+    if (loginBtn && container) {
+        loginBtn.addEventListener('click', () => {
+            container.classList.remove("active");
+        });
+    }
+});
